18203e6cb36e9ae6828c39da959fd96b41278cfc
XenDev PH <adagunan11@gmail.com>
2023-06-01 20:50:22 +0800

    🎨 Initial commit

---
+++ navigation/LinkingConfiguration.tsx
@@ -0,0 +1,31 @@
+/**
+ * Learn more about deep linking with React Navigation
+ * https://reactnavigation.org/docs/deep-linking
+ * https://reactnavigation.org/docs/configuring-links
+ */
+import * as Linking from "expo-linking";
+
+const LinkingConfiguration: any = {
+  prefixes: [Linking.createURL("/")],
+  config: {
+    screens: {
+      Root: {
+        screens: {
+          TabOne: {
+            screens: {
+              TabOneScreen: "one",
+            },
+          },
+          TabTwo: {
+            screens: {
+              TabTwoScreen: "two",
+            },
+          },
+        },
+      },
+      NotFound: "*",
+    },
+  },
+};
+
+export default LinkingConfiguration;
